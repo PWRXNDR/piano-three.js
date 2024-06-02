cdfc92b5914674724827c0a9d50419c3ff513bb7
Alex <124251221+PWRXNDR@users.noreply.github.com>
2024-06-02 22:43:19 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default {
+    build: {
+        outDir: '../dist'
+    },
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    plugins: [
+        wasm(),
+        topLevelAwait()
+    ]
+}
